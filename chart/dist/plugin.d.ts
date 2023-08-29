b47dab9dcd222f29c6d2ecd4b9ae50f2ff444949
DESKTOP-L1774LF\owner <jhpark@pluxity.com>
2023-08-29 09:12:53 +0900

    first commit

---
+++ chart/dist/plugin.d.ts
@@ -0,0 +1,12 @@
+import { Chart } from 'chart.js';
+export declare class ChartDatasourcePrometheusPlugin {
+    id: string;
+    beforeInit(chart: Chart, options: any): void;
+    afterInit(chart: Chart, args: any, _options: any): void;
+    beforeUpdate(chart: Chart, args: any, _options: any): boolean;
+    afterDraw(chart: Chart, args: any, _options: any): void;
+    updateMessage(chart: Chart, _options: any): void;
+    writeText(chart: Chart, message: string, fn?: (ctx: CanvasRenderingContext2D) => void): void;
+    destroy(chart: Chart, args: any, _options: any): void;
+    private resumeRendering;
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+[ windows_exporter 다운로드 ]
+https://github.com/prometheus-community/windows_exporter/releases
+
+
+[ Grafana Dashboard ]
+Node Exporter for Prometheus Dashboard - 11074
+Node Exporter Full - 1860
+
+windows_exporter for Prometheus Dashboard - 13466
